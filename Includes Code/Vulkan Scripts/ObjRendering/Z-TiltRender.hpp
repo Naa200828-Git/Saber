809385d00b0dc7e3dbcd70178f3e778c4c50232a
Naa200828-Git <naa.200828@gmail.com>
2025-01-20 13:30:58 -0500

    asdasd

--- Includes Code/Vulkan Scripts/ObjRendering/Z-TiltRender.hpp
+++ Includes Code/Vulkan Scripts/ObjRendering/Z-TiltRender.hpp
@@ -10,19 +10,21 @@
 int PosX, PosY,
     CameraX, CameraY_TopDown; 
 
-inline void Get_LTSprite_SizeDeclartions() // change name to [...] get amounts
+inline unsigned char Get_LTSprite_SizeDeclartions() // change name to [...] get amounts, Seperate 
 {// If Moved
     // Search Through chunk  Of Close By Allocated Objects, Count Amount of !nullptrs
     // The Stack buffer
 
     // if num > max ... OverFlow handle
+    // return ;
 }
 inline void Render_LTSprite(
     const unsigned char& UniqueObj_Amount, 
     const LTSprite UniqueObj_Buff[ccUniqueObj_BuffSize], 
     const unsigned char ObjPos_BuffAmount[ccObjPos_BuffSize],
     const unsigned char ObjPosX_Buff[ccObjPos_BuffSize],
-    const unsigned char ObjPosY_Buff[ccObjPos_BuffSize]
+    const unsigned char ObjPosY_Buff[ccObjPos_BuffSize],
+    ...
 ){// If Moved
     // If Objects On Screen
     for (unsigned char o = 0; o < UniqueObj_Amount; o++)
@@ -48,9 +50,16 @@ inline void LTSprite_Once()
     unsigned char* ObjPosX_Overflow = new unsigned char[1],
         * ObjPosY_Overflow = new unsigned char[1],
         * ObjPos_Amount_Overflow = new unsigned char[1];
-#endif
+
 // ~~~~ Single Loop For Test
-    Render_LTSprite(UniqueObj_Amount, UniqueObj_Buff, ObjPos_BuffAmount,ObjPosX_Buff,ObjPosY_Buff);
+
+// ~~~~ Handle Overflow
+
+    UniqueObj_Amount = 0/*Get Num*/;// Per instance
+    if (UniqueObj_Amount > ccUniqueObj_BuffSize)
+
+#endif
+    Render_LTSprite(UniqueObj_Amount, UniqueObj_Buff, ObjPos_BuffAmount, ObjPosX_Buff, ObjPosY_Buff);
 }
 
 #endif
