3f4fc57b7e5cb9d982c32b16a522196064179b6b
Naa200828-Git <naa.200828@gmail.com>
2025-01-22 10:50:34 -0500

    alot of stuff fr gang :))))))

--- CCL/CCL_Darr/CCL_Darr.hpp
+++ CCL/CCL_Darr/CCL_Darr.hpp
@@ -3,7 +3,7 @@
 
 template <typename Type> struct CCL_Darr {
     Type* Data = nullptr; // Pointer to dynamically allocated memory
-    unsigned short int Size = 5;    // Default size
+    unsigned short int Size = 1;    // Default size
 };
 
 #endif
--- MazeGenerator.cpp
+++ MazeGenerator.cpp
@@ -7,6 +7,11 @@
 
 #include <ctime>
 
+#define Sized_Int unsigned int    
+constexpr const Sized_Int ccMaze_SizeX = 100,
+    ccMaze_SizeY = 150,
+    MaxPath_Size = (ccMaze_SizeX + ccMaze_SizeY / 2) * 0.1;   
+
 unsigned int CCL_XOR_13L17R5L_32_t(const unsigned int cMax) {
     unsigned int Seed = (unsigned int)(std::time(NULL));
     Seed ^= Seed << 13;
@@ -19,12 +24,43 @@ bool CCL_TF() {  // Maybe Make inline
     return Seed & 0b1;
 }
 
-int main()
+struct alignas(1) sPath
+{
+    Sized_Int Length;      
+    unsigned char Direction;
+};
+    
+struct CCL_Darr {
+    sPath* Data = nullptr; // Pointer to dynamically allocated memory
+    unsigned short int Size = 1;    // Default size
+}; 
+void CCL_Darr_cAdjSize(CCL_Darr& Arr, const unsigned char cAdjSize) {
+    const unsigned short int NewSize = Arr.Size + cAdjSize; // Increase size
+    sPath* NewData = new sPath[NewSize]; // Allocate new memory
+
+    // Copy existing elements to new memory
+    for (unsigned short int i = 0; i < Arr.Size; ++i) 
+        NewData[i] = Arr.Data[i];
+
+    // Initialize new elements
+    for (unsigned short int i = Arr.Size; i < NewSize; ++i) 
+        NewData[i] = sPath();
+
+    // Free old memory
+    delete[] Arr.Data;
+
+    // Update the array
+    Arr.Data = NewData;
+    Arr.Size = NewSize;
+}
+void CCL_Darr_Free(CCL_Darr& Arr) {
+    delete[] Arr.Data; // Free the memory
+    Arr.Data = nullptr;
+    Arr.Size = 0;
+};
+
+inline bool GenerateMaze()
 {   
-    #define Sized_Int unsigned int    
-    constexpr const Sized_Int ccMaze_SizeX = 100,
-        ccMaze_SizeY = 150,
-        MaxPath_Size = (ccMaze_SizeX + ccMaze_SizeY / 2) * 0.1;   
 // ~~~~ Choose End Points
     Sized_Int Connected = CCL_TF();
 
@@ -45,62 +81,33 @@ int main()
         Path = CCL_XOR_13L17R5L_32_t(MaxPath_Size);
     signed char Direction = CCL_XOR_13L17R5L_32_t(4);
 
-    struct sPath
-    {
-        Sized_Int Length;
-        unsigned char Direction;
-    };
-    
-    struct CCL_Full_Darr { // split into sperate functions that take & params for functions
-        sPath* Data;
-        Sized_Int Size;
-        Sized_Int Capacity;
-
-        CCL_Full_Darr() : Data(nullptr), Size(0), Capacity(0) {}
-
-        ~CCL_Full_Darr() {
-            delete[] Data;
-        }   
-
-        void push_back(const sPath& Item) 
-        {
-            if (Size == Capacity) 
-            {
-                Capacity = (Capacity == 0) ? 1 : Capacity * 2;
-                sPath* newData = new sPath[Capacity];
-                
-                for (Sized_Int i = 0; i < Size; ++i) 
-                    newData[i] = Data[i];
-
-                delete[] Data;
-                Data = newData;
-            }
-            Data[Size++] = Item;
-        }
-
-        sPath& operator[](Sized_Int Index) {
-            return Data[Index];
-        }
-    };
-
-    CCL_Full_Darr PathFromStart,
+    CCL_Darr PathFromStart,
         PathFromEnd;
 
-    while (Connected) // maybe put reset at begining and do not init with reset functions
+    while (1) // maybe put reset at begining and do not init with reset functions
     {
 // ~~~~ Fill Path
         // Fill 
         // set PathPos's
 // ~~~~ Reset
-        PathFromStart[Connected].Length = CCL_XOR_13L17R5L_32_t(MaxPath_Size);
-        PathFromStart[Connected].Direction = CCL_XOR_13L17R5L_32_t(4);
+        CCL_Darr_cAdjSize(PathFromStart, 1);
 
-        PathFromStart[Connected].Length = CCL_XOR_13L17R5L_32_t(MaxPath_Size);
-        PathFromStart[Connected].Direction = CCL_XOR_13L17R5L_32_t(4);
+        PathFromStart.Data[Connected].Length = CCL_XOR_13L17R5L_32_t(MaxPath_Size);
+        PathFromStart.Data[Connected].Direction = CCL_XOR_13L17R5L_32_t(4);
+
+        CCL_Darr_cAdjSize(PathFromEnd, 1);
+
+        PathFromEnd.Data[Connected].Length = CCL_XOR_13L17R5L_32_t(MaxPath_Size);
+        PathFromEnd.Data[Connected].Direction = CCL_XOR_13L17R5L_32_t(4);
 
         if (PathPosX[0], PathPosX[1], PathPosY[0], PathPosY[1]) break; // If Connected
         Connected++;
     }
 // ~~~~ Maze Finished Generating
+    return 0;
 }
 
+int main()
+{
+    return GenerateMaze();
+}
